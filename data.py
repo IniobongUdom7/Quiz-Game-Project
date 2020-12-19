b55f505da8e17ea552031b00ae5791ff26bc5f90
Iniobong Udom <udompowerini@gmail.com>
2020-12-19 16:22:54 +0000

    Initial commit

---
+++ data.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "general: Current affairs",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Abuja is the capital of Nigeria.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "General: General",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Arctic is the continent in the world.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Science",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Vatican City is with only 0.44 sq.km.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Cartoon",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Cinderella was the first Disney princess.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "General: General",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": " Football team has 11 players in a team.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: General",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Marrakesh is the capital of Morocco.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "General: General",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "M&M stands for Mars and Moordale.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Animal",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The unicorn is the national animal of Scotland.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Google was originally called 'Backrub'.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Animal",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "A lion's roar can be heard up to eight kilometres away.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    }
+]
